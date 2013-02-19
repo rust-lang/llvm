f1846ee6222f981a00eebc347dd76550fd05fcd2
Derek Schuff <dschuff@chromium.org>
2013-02-19 15:13:36 -0800

    Move ABI type checker to its own file
    R=jvoung@chromium.org,eliben@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/12302030
    
    (cherry picked from commit 06dd2371667be256a846a68e51d376349f041752)
    
    Conflicts:
    	lib/Analysis/NaCl/CheckTypes.h

--- lib/Analysis/NaCl/PNaClABIVerifyModule.cpp
+++ lib/Analysis/NaCl/PNaClABIVerifyModule.cpp
@@ -21,7 +21,7 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Analysis/NaCl.h"
 
-#include "CheckTypes.h"
+#include "PNaClABITypeChecker.h"
 using namespace llvm;
 
 namespace {
@@ -31,11 +31,11 @@ class PNaClABIVerifyModule : public ModulePass {
   static char ID;
   PNaClABIVerifyModule();
   bool runOnModule(Module &M);
-  virtual void print(llvm::raw_ostream &O, const Module *M) const;
+  virtual void print(raw_ostream &O, const Module *M) const;
  private:
-  TypeChecker TC;
+  PNaClABITypeChecker TC;
   std::string ErrorsString;
-  llvm::raw_string_ostream Errors;
+  raw_string_ostream Errors;
 };
 
 static const char *linkageName(GlobalValue::LinkageTypes LT) {
@@ -77,14 +77,15 @@ bool PNaClABIVerifyModule::runOnModule(Module &M) {
       // GVs are pointers, so print the pointed-to type for clarity
       Errors << "Variable " + MI->getName() +
           " has disallowed type: " +
-          TypeChecker::getTypeName(MI->getType()->getContainedType(0)) + "\n";
+          PNaClABITypeChecker::getTypeName(MI->getType()->getContainedType(0))
+          + "\n";
     } else if (MI->hasInitializer()) {
       // If the type of the global is bad, no point in checking its initializer
       Type *T = TC.checkTypesInValue(MI->getInitializer());
       if (T)
         Errors << "Initializer for " + MI->getName() +
             " has disallowed type: " +
-            TypeChecker::getTypeName(T) + "\n";
+            PNaClABITypeChecker::getTypeName(T) + "\n";
     }
 
     // Check GV linkage types
