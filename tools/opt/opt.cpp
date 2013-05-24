d622ca00ee568f87271e4b1a20af4a7c8a1d1383
Mark Seaborn <mseaborn@chromium.org>
2013-05-24 12:56:47 -0700

    PNaCl: Add pass to expand out Clang-generated umul.with.overflow calls
    This adds a pass, ExpandMulWithOverflow, to expand out the
    llvm.umul.with.overflow calls that Clang generates to implement an
    overflow check for C++'s new[] operator.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3434
    TEST=expand-mul-with-overflow.ll
    
    Review URL: https://codereview.chromium.org/14649027
    
    (cherry picked from commit de078f629ea70df5987a28390c1d3a0da5c844d5)

--- tools/opt/opt.cpp
+++ tools/opt/opt.cpp
@@ -426,6 +426,7 @@ int main(int argc, char **argv) {
   initializeExpandConstantExprPass(Registry);
   initializeExpandCtorsPass(Registry);
   initializeExpandGetElementPtrPass(Registry);
+  initializeExpandMulWithOverflowPass(Registry);
   initializeExpandTlsPass(Registry);
   initializeExpandTlsConstantExprPass(Registry);
   initializeExpandVarArgsPass(Registry);
