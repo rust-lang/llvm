ba00c8a27962dc445a122860c3add2a6b9bef7b2
Mark Seaborn <mseaborn@chromium.org>
2013-05-30 13:08:26 -0700

    PNaCl: Add a pass to expand out Clang's use of registers of struct type
    Clang's implementation of C++ method pointers generates IR that uses
    LLVM registers with struct type -- specifically, loads and stores of
    struct values, and extractvalue instructions.  See
    lib/CodeGen/ItaniumCXXABI.cpp in Clang.  Add a pass, ExpandStructRegs,
    which expands out those uses.
    
    Factor out a function from ExpandArithWithOverflow so that the two
    passes can share some code.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3343
    TEST=*.ll tests + trybots + GCC torture tests
    
    Review URL: https://codereview.chromium.org/15692014
    
    (cherry picked from commit f72e0b53e25548d6db9220a03a303e589c9773a4)

--- tools/opt/opt.cpp
+++ tools/opt/opt.cpp
@@ -442,6 +442,7 @@ int main(int argc, char **argv) {
   initializeExpandCtorsPass(Registry);
   initializeExpandGetElementPtrPass(Registry);
   initializeExpandSmallArgumentsPass(Registry);
+  initializeExpandStructRegsPass(Registry);
   initializeExpandTlsPass(Registry);
   initializeExpandTlsConstantExprPass(Registry);
   initializeExpandVarArgsPass(Registry);
