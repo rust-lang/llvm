emscripten-core/emscripten-fastcomp

Add CL argument -bitcode-format to llvm-dis, llc, and pnacl-llc.
BUG= https://code.google.com/p/nativeclient/issues/detail?id=3469
R=jvoung@chromium.org

Review URL: https://codereview.chromium.org/16385002

tools/llvm-dis/llvm-dis.cpp
@@ -19,10 +19,12 @@
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/Assembly/AssemblyAnnotationWriter.h"
 #include "llvm/Bitcode/ReaderWriter.h"
+#include "llvm/Bitcode/NaCl/NaClReaderWriter.h"  // @LOCALMOD
 #include "llvm/DebugInfo.h"
 #include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
+#include "llvm/IRReader/IRReader.h"  // @LOCALMOD
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/DataStream.h"
 #include "llvm/Support/FormattedStream.h"
@@ -56,6 +58,16 @@ ShowAnnotations("show-annotations",
 // (includes output format, soname, and dependencies).
 static cl::opt<bool>
 DumpMetadata("dump-metadata", cl::desc("Dump bitcode metadata"));
+
+static cl::opt<NaClFileFormat>
+InputFileFormat(
+    "bitcode-format",
+    cl::desc("Define format of input bitcode file:"),
+    cl::values(
+        clEnumValN(LLVMFormat, "llvm", "LLVM bitcode file (default)"),
+        clEnumValN(PNaClFormat, "pnacl", "PNaCl bitcode file"),
+        clEnumValEnd),
+    cl::init(LLVMFormat));
 // @LOCALMOD-END
 
 namespace {
@@ -140,8 +152,22 @@ int main(int argc, char **argv) {
       DisplayFilename = "<stdin>";
     else
       DisplayFilename = InputFilename;
-    M.reset(getStreamedBitcodeModule(DisplayFilename, streamer, Context,
-                                     &ErrorMessage));
+
+    // @LOCALMOD-BEGIN
+    switch (InputFileFormat) {
+      case LLVMFormat:
+        M.reset(getStreamedBitcodeModule(DisplayFilename, streamer, Context,
+                                         &ErrorMessage));
+        break;
+      case PNaClFormat:
+        M.reset(getNaClStreamedBitcodeModule(DisplayFilename, streamer, Context,
+                                             &ErrorMessage));
+        break;
+      default:
+        ErrorMessage = "Don't understand specified bitcode format";
+        break;
+    }
+    // @LOCALMOD-END
     if(M.get() != 0 && M->MaterializeAllPermanently(&ErrorMessage)) {
       M.reset();
     }

rust-lang/llvm

(cherry picked from commit 098dc634a5e6458a319617ec1737868cbf0f3d01)
I, Richard Diamond, also took the liberty of merging NaClParseIR into ParseIR.
Conflicts:
	include/llvm/IRReader/IRReader.h
	lib/CMakeLists.txt
	lib/IRReader/IRReader.cpp
	lib/IRReader/LLVMBuild.txt
	lib/IRReader/Makefile
	lib/LLVMBuild.txt
	lib/Makefile
	tools/llc/CMakeLists.txt
	tools/llc/LLVMBuild.txt
	tools/llc/Makefile
	tools/llc/llc.cpp
	tools/llvm-dis/CMakeLists.txt
	tools/llvm-dis/llvm-dis.cpp
	tools/opt/opt.cpp
	tools/pnacl-llc/CMakeLists.txt
	tools/pnacl-llc/LLVMBuild.txt
	tools/pnacl-llc/Makefile
	tools/pnacl-llc/pnacl-llc.cpp

tools/llvm-dis/llvm-dis.cpp
@@ -18,11 +18,13 @@
 
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/Bitcode/ReaderWriter.h"
+#include "llvm/Bitcode/NaCl/NaClReaderWriter.h"  // @LOCALMOD
 #include "llvm/DebugInfo.h"
 #include "llvm/IR/AssemblyAnnotationWriter.h"
 #include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
+#include "llvm/IRReader/IRReader.h"  // @LOCALMOD
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/DataStream.h"
 #include "llvm/Support/FormattedStream.h"
@@ -51,6 +53,16 @@ static cl::opt<bool>
 ShowAnnotations("show-annotations",
                 cl::desc("Add informational comments to the .ll file"));
 
+
+static cl::opt<FileFormat>
+InputFileFormat(
+    "bitcode-format",
+    cl::desc("Define format of input bitcode file:"),
+    cl::values(
+        clEnumValN(LLVMFormat, "llvm", "LLVM bitcode file (default)"),
+        clEnumValN(PNaClFormat, "pnacl", "PNaCl bitcode file"),
+        clEnumValEnd),
+    cl::init(LLVMFormat));
 namespace {
 
 static void printDebugLoc(const DebugLoc &DL, formatted_raw_ostream &OS) {
@@ -133,8 +145,21 @@ int main(int argc, char **argv) {
       DisplayFilename = "<stdin>";
     else
       DisplayFilename = InputFilename;
-    M.reset(getStreamedBitcodeModule(DisplayFilename, streamer, Context,
-                                     &ErrorMessage));
+
+    switch (InputFileFormat) {
+      case LLVMFormat:
+        M.reset(getStreamedBitcodeModule(DisplayFilename, streamer, Context,
+                                         &ErrorMessage));
+        break;
+      case PNaClFormat:
+        M.reset(getNaClStreamedBitcodeModule(DisplayFilename, streamer, Context,
+                                             &ErrorMessage));
+        break;
+      default:
+        ErrorMessage = "Don't understand specified bitcode format";
+        break;
+    }
+
     if(M.get() != 0) {
       if (error_code EC = M->materializeAllPermanently()) {
         ErrorMessage = EC.message();