001d6b9e5fe4ef17c94b4ae9c48c27e7044c4df0
Richard Diamond <wichard@hahbee.co>
2014-02-27 11:52:24 -0600

    Rename building_lib -> BuildingLib.

--- include/llvm/Transforms/NaCl.h
+++ include/llvm/Transforms/NaCl.h
@@ -56,7 +56,7 @@ ModulePass *createRewritePNaClLibraryCallsPass();
 ModulePass *createStripAttributesPass();
 ModulePass *createStripMetadataPass();
 
-void PNaClABISimplifyAddPreOptPasses(PassManagerBase &PM, const bool building_lib = false);
+void PNaClABISimplifyAddPreOptPasses(PassManagerBase &PM, const bool BuildingLib = false);
 void PNaClABISimplifyAddPostOptPasses(PassManagerBase &PM);
 
 Instruction *PhiSafeInsertPt(Use *U);
